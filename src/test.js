b04ae28512f61516dc8b88c3b57fcfb5711a03bb
jmechristian <jmechristian@gmail.com>
2023-06-26 16:39:21 -0400

    ssss

--- src/test.js
+++ src/test.js
@@ -1,40 +1,40 @@
 export const data = [
   {
     name: 'test_1',
-    prompt: '1. Sign up and into a Packaging School profile',
+    prompt: '1) Sign up and into a Packaging School profile',
   },
   {
     name: 'test_2',
     prompt:
-      '1. Save 2 design courses and 3 materials courses to your user profile from the All Courses page. 2. Then unfavorite 2 of them.',
+      '1) Save two design courses and three materials courses to your user profile from the All Courses page. 2) Then unfavorite two of them.',
   },
   {
     name: 'test_3',
     prompt:
-      '1. Sign out of your account. 2. Start a CMPM Application. 3. Choose to either SAVE or complete and SUBMIT the application. (Try to submit without the form being complete. Stripe payment can be ignored.)',
+      '1) Sign out of your account. 2) Start a CMPM Application. 3) Choose to either SAVE or complete and SUBMIT the application. (Try to submit without the form being complete. Stripe payment can be ignored.)',
   },
   {
     name: 'test_4',
     prompt:
-      '1. Start a CPS application from your user profile. 2. Save Application. 3. Return to your Profile and ensure you now have the option to "View" the CPS application from your Dashboard.',
+      '1) Start a CPS application from your user profile. 2) Save Application. 3) Return to your Profile and ensure you now have the option to "View" the CPS application from your Dashboard.',
   },
   {
     name: 'test_5',
-    prompt: '1. Edit your profile information. 2. Save and exit.',
+    prompt: '1) Edit your profile information. 2) Save and exit.',
   },
   {
     name: 'test_6',
     prompt:
-      '1. Search for a Learning of the Month on "sustainability". 2. Share the LOTM on social (No need to complete the share, just check that the preview aligns with the content.)',
+      '1) Search for a Learning of the Month on "sustainability". 2) Share the LOTM on social (No need to complete the share, just check that the preview aligns with the content.)',
   },
   {
     name: 'test_7',
     prompt:
-      '1. Search for a resource on "packaging materials". 2. Share the resource on social (No need to complete the share, just check that the preview aligns with the content.)',
+      '1) Search for a resource on "packaging materials". 2) Share the resource on social (No need to complete the share, just check that the preview aligns with the content.)',
   },
   {
     name: 'test_8',
-    prompt: '1. Find 3 FREE courses.  2. Save them to your user profile.',
+    prompt: '1) Find three FREE courses.  2) Save them to your user profile.',
   },
   {
     name: 'test_9',
