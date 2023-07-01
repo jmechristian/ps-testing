2d34ad4fc56f8d109306bfd6a91d9cd41fc99f15
jmechristian <jmechristian@gmail.com>
2023-06-30 20:00:26 -0400

    ssss

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,23 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Dashboard from './Dashboard.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
 import Layout from './components/Layout.jsx';
 import Results from './components/Results.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-  {
-    path: '/results',
-    element: <Results />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Layout>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='results' element={<Results />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   </Provider>
 );
