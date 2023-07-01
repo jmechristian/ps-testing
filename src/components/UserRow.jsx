8f4efaf9758cf3f351264737bfc6fe32f63e36aa
jmechristian <jmechristian@gmail.com>
2023-06-30 22:22:51 -0400

    sssdd

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -10,69 +10,47 @@ const UserRow = ({ user }) => {
       </div>
       <div className='grid grid-cols-11 w-full items-start'>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 1
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>1</div>
           <UserRowItem test={user.test1} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 2
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>2</div>
           <UserRowItem test={user.test2} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 3
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>3</div>
           <UserRowItem test={user.test3} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 4
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>4</div>
           <UserRowItem test={user.test4} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 5
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>5</div>
           <UserRowItem test={user.test5} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 6
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>6</div>
           <UserRowItem test={user.test6} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 7
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>7</div>
           <UserRowItem test={user.test7} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 8
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>8</div>
           <UserRowItem test={user.test8} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 9
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>9</div>
           <UserRowItem test={user.test9} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 10
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>10</div>
           <UserRowItem test={user.test10} />
         </div>
         <div className='flex flex-col gap-3 justify-center items-center'>
-          <div className='uppercase text-xs md:text-sm text-gray-500'>
-            Test 11
-          </div>
+          <div className='uppercase text-xs md:text-sm text-gray-500'>11</div>
           <UserRowItem test={user.test11} />
         </div>
         {/* <div className='flex flex-col gap-3 justify-center items-center'>
